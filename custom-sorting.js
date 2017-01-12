9a7cf6387eeafe6a94323dc70af9b194958f8941
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-12 19:04:54 +0000

    Implemented custom sort of students array, by name and descending age

--- custom-sorting.js
+++ custom-sorting.js
@@ -2,13 +2,30 @@
 
 var array = [10, 2, 5, 1, 9];
 
-
-function sortFunction (a, b) {
+array.sort(function(a, b) {
   return (a - b);
-}
+});
 
-array.sort(sortFunction);
 console.log(array);
 
 
 // CUSTOM COMPARATOR
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return (b.age - a.age);
+});
+
+console.log(students);
